fd350e7a657342bdeb24d380db5b7782fc8e54df
lazyboson <lazyboson@lazyboson@gmail.com>
2023-04-08 00:01:44 +0530

    added rand numb

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number");
-    println!("Please input your guess");
+    
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Failed to read the line");
-    println!("You guessed: {guess}")
+    println!("You guessed: {guess}");
+    let x = 5; 
+    let y = 6;
+    println!("x= {x} and y+2 = {}", y);
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The Secret number is: {}", secret_number);
 }
