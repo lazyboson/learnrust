a7331088095cb2641ac162cdded9b7894f9d4b54
lazyboson <lazyboson@lazyboson@gmail.com>
2023-04-08 00:09:57 +0530

    added few val

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{io, cmp::Ordering};
 use rand::Rng;
 
 fn main() {
@@ -11,6 +11,13 @@ fn main() {
     let y = 6;
     println!("x= {x} and y+2 = {}", y);
 
+    let guessed_num = 92;
+
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The Secret number is: {}", secret_number);
+    match guessed_num.cmp(&secret_number) {
+       Ordering::Less => println!("number is small"),
+       Ordering::Equal => println!("number is equal"),
+       Ordering::Greater => println!("number is greater"), 
+    }
 }
